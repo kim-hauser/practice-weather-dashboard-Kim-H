1d9ad22683c8dccb863c48ef3fac921f9669659f
kim-hauser <knhauser1s@gmail.com>
2026-08-05 16:50:14 -0500

    Initialize Spring Boot weather dashboard

---
+++ weatherdashboard/src/main/java/com/example/weatherdashboard/WeatherdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherdashboardApplication.class, args);
+	}
+
+}
